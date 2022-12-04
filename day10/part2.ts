ac7032e41cdae68a925060236d40af87acbe6ff6
colindembovsky <colindembovsky@gmail.com>
2022-12-04 10:24:09 -0600

    Solve 10.2

--- day10/part2.ts
+++ day10/part2.ts
@@ -9,7 +9,22 @@ function readFile(fileName: string): string {
 
 let contents = readFile(`${ROOT_DIR}/input.txt`);
 //let contents = readFile(`${ROOT_DIR}/input.txt`);
-//console.log(contents);
+let adapters = contents.split("\n").map((line) => parseInt(line)).sort((a, b) => a - b);
+adapters.unshift(0);
+adapters.push(adapters[adapters.length - 1] + 3);
 
 console.log("==== PART 2 ====");
-
+let waysToGetTo = new Map<number, number>();
+waysToGetTo.set(0, 1);
+for (let i = 0; i < adapters.length; i++) {
+    let adapter = adapters[i];
+    let ways = waysToGetTo.get(adapter) || 0;
+    for (let j = i + 1; j < adapters.length; j++) {
+        let nextAdapter = adapters[j];
+        if (nextAdapter - adapter > 3) {
+            break;
+        }
+        waysToGetTo.set(nextAdapter, (waysToGetTo.get(nextAdapter) || 0) + ways);
+    }
+}
+console.log(`Result: ${waysToGetTo.get(adapters[adapters.length - 1])}`);
