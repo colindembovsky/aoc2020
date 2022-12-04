7a7018113a5f73cbeb127141da41da72a1f62e97
colindembovsky <colindembovsky@gmail.com>
2022-12-04 10:11:48 -0600

    Solve 10.1

--- day10/part1.ts
+++ day10/part1.ts
@@ -6,11 +6,15 @@ const ROOT_DIR="day10";
 function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
+let contents = readFile(`${ROOT_DIR}/input.txt`);
 
 console.log("==== PART 1 ====");
-let contents = readFile(`${ROOT_DIR}/test-input.txt`);
-
-
-console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
-
+let adapters = contents.split("\n").map((line) => parseInt(line)).sort((a, b) => a - b);
+adapters.unshift(0);
+adapters.push(adapters[adapters.length - 1] + 3);
+let diffs = new Map<number, number>();
+for (let i = 0; i < adapters.length - 1; i++) {
+    let diff = adapters[i + 1] - adapters[i];
+    diffs.set(diff, (diffs.get(diff) || 0) + 1);
+}
+console.log(`Result: ${diffs.get(1)! * diffs.get(3)!}`);
