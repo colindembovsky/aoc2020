c4cc67dcb5fd3a08ce10b4f8826f9109f79c40d6
colindembovsky <colindembovsky@gmail.com>
2022-12-03 23:53:13 -0600

    Solve day9.1

--- day09/part1.ts
+++ day09/part1.ts
@@ -7,10 +7,40 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
-console.log("==== PART 1 ====");
-let contents = readFile(`${ROOT_DIR}/test-input.txt`);
+class XMAS {
+    constructor(public preamble: number, public data: number[]) {
+    }
 
+    public findInvalidNumber(): number {
+        for (let i = this.preamble; i < this.data.length; i++) {
+            const range = this.data.slice(i - this.preamble, i);
+            const target = this.data[i];
+            if (!this.isValid(range, target)) {
+                return target;
+            }
+        }
+        return -1;
+    }
+
+    private isValid(range: number[], target: number): boolean {
+        for (let i = 0; i < range.length; i++) {
+            for (let j = i + 1; j < range.length; j++) {
+                if (range[i] + range[j] === target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+//let contents = readFile(`${ROOT_DIR}/test-input.txt`);
+let contents = readFile(`${ROOT_DIR}/input.txt`);
+let nums = contents.split("\n").map((s) => parseInt(s));
+
+console.log("==== PART 1 ====");
+let xmas1 = new XMAS(25, nums);
+console.log(xmas1.findInvalidNumber());
 
 console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
 
