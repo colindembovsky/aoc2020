374ad8d829d548ea6edd880846880655b185c20d
colindembovsky <colindembovsky@gmail.com>
2022-12-05 09:03:45 -0600

    Solve day 12

--- day12/part1.ts
+++ day12/part1.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import { emit } from "process";
 
 const ROOT_DIR="day12";
 
@@ -7,10 +8,51 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
+class Ship {
+    constructor(public east: number, public north: number, public facing: number) {
+    }
+
+    public move(line: string) {
+        const dir = line[0];
+        const dist = parseInt(line.substring(1));
+        this.doMove(dir, dist);
+    }
+
+    private doMove(dir: string, dist: number) {
+        if (dir == "N") {
+            this.north += dist;
+        } else if (dir === "S") {
+            this.north -= dist;
+        } else if (dir === "E") {
+            this.east += dist;
+        } else if (dir === "W") {
+            this.east -= dist;
+        } else if (dir === "L") {
+            this.facing = (this.facing - dist + 360) % 360;
+        } else if (dir === "R") {
+            this.facing = (this.facing + dist) % 360;
+        } else if (dir === "F") {
+            if (this.facing === 0) {
+                this.north += dist;
+            } else if (this.facing === 90) {
+                this.east += dist;
+            } else if (this.facing === 180) {
+                this.north -= dist;
+            } else if (this.facing === 270) {
+                this.east -= dist;
+            }
+        }
+    }
+
+    manhattanDistance(): number {
+        return Math.abs(this.east) + Math.abs(this.north);
+    }
+}
+
+let contents = readFile(`${ROOT_DIR}/input.txt`);
+let lines = contents.split("\n");
+
 console.log("==== PART 1 ====");
-let contents = readFile(`${ROOT_DIR}/test-input.txt`);
-
-
-console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
-
+let ship = new Ship(0, 0, 90);
+lines.forEach(l => ship.move(l));
+console.log(ship.manhattanDistance());
--- day12/part2.ts
+++ day12/part2.ts
@@ -7,9 +7,57 @@ function readFile(fileName: string): string {
     return fs.readFileSync(fileName, "utf8");
 }
 
+class Ship {
+    wpEast: number = 10;
+    wpNorth: number = 1;
+
+    constructor(public east: number, public north: number, public facing: number) {
+    }
+
+    public move(line: string) {
+        const dir = line[0];
+        const dist = parseInt(line.substring(1));
+        this.doMove(dir, dist);
+    }
+
+    private doMove(dir: string, dist: number) {
+        if (dir == "N") {
+            this.wpNorth += dist;
+        } else if (dir === "S") {
+            this.wpNorth -= dist;
+        } else if (dir === "E") {
+            this.wpEast += dist;
+        } else if (dir === "W") {
+            this.wpEast -= dist;
+        } else if (dir === "L") {
+            this.rotateWaypoint(dist);
+        } else if (dir === "R") {
+            this.rotateWaypoint(-dist);
+        } else if (dir === "F") {
+            this.east += this.wpEast * dist;
+            this.north += this.wpNorth * dist;
+        }
+    }
+
+    private rotateWaypoint(degrees: number) {
+        const radians = degrees * Math.PI / 180;
+        const cos = Math.cos(radians);
+        const sin = Math.sin(radians);
+        const x = this.wpEast * cos - this.wpNorth * sin;
+        const y = this.wpEast * sin + this.wpNorth * cos;
+        this.wpEast = Math.round(x);
+        this.wpNorth = Math.round(y);
+    }
+
+    manhattanDistance(): number {
+        return Math.abs(this.east) + Math.abs(this.north);
+    }
+}
+
 let contents = readFile(`${ROOT_DIR}/input.txt`);
-//let contents = readFile(`${ROOT_DIR}/input.txt`);
-//console.log(contents);
+let lines = contents.split("\n");
 
 console.log("==== PART 2 ====");
-
+let ship = new Ship(0, 0, 90);
+lines.forEach(l => ship.move(l));
+console.log(ship.manhattanDistance());
