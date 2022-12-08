dec26e4e2c1e947d65dc21ba6d3052df4cc21647
colindembovsky <colindembovsky@gmail.com>
2022-12-08 14:30:15 -0600

    Solve 13.1

--- day13/part1.ts
+++ day13/part1.ts
@@ -2,15 +2,27 @@ import * as fs from "fs";
 
 const ROOT_DIR="day13";
 
-// read in a file and print it out to the console
-function readFile(fileName: string): string {
-    return fs.readFileSync(fileName, "utf8");
+const realSchedule = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,859,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,373,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37";
+const realStart = 1001798;
+
+const testSchedule = "7,13,x,x,59,x,31,19";
+const testStart = 939;
+
+const realBusses = realSchedule.split(",").filter(b => b !== "x").map(b => parseInt(b));
+const testBusses = testSchedule.split(",").filter(b => b !== "x").map(b => parseInt(b));
+
+function earliestBus(start: number, busses: number[]): { bus: number, wait: number } {
+    let earliest = { bus: 0, wait: Number.MAX_SAFE_INTEGER };
+    for (const bus of busses) {
+        const nextArrivalWait = Math.ceil(start / bus) * bus - start;
+        if (nextArrivalWait < earliest.wait) {
+            earliest = { bus, wait: nextArrivalWait };
+        }
+    }
+    return earliest;
 }
 
 console.log("==== PART 1 ====");
-let contents = readFile(`${ROOT_DIR}/test-input.txt`);
-
-
-console.log("==== PART 2 ====");
-contents = readFile(`${ROOT_DIR}/input.txt`);
-
+//const earliest = earliestBus(testStart, testBusses);
+const earliest = earliestBus(realStart, realBusses);
+console.log(`Earliest bus: ${earliest.bus}, wait: ${earliest.wait}, product: ${earliest.bus * earliest.wait}`);
--- day13/part2.ts
+++ day13/part2.ts
@@ -2,14 +2,13 @@ import * as fs from "fs";
 
 const ROOT_DIR="day13";
 
-// read in a file and print it out to the console
-function readFile(fileName: string): string {
-    return fs.readFileSync(fileName, "utf8");
-}
+const realSchedule = "19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,859,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,373,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37";
+const testSchedule = "7,13,x,x,59,x,31,19";
 
-let contents = readFile(`${ROOT_DIR}/input.txt`);
-//let contents = readFile(`${ROOT_DIR}/input.txt`);
-//console.log(contents);
+const realBusses = realSchedule.split(",").filter(b => b !== "x").map(b => parseInt(b));
+const testBusses = testSchedule.split(",").filter(b => b !== "x").map(b => parseInt(b));
+
+
 
 console.log("==== PART 2 ====");
-
+//const earliest = earliestBus(testStart, testBusses);
